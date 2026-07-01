d3de4755cfd2a11136c575cc485706e412b8d876
Light <light.diamonds@physci.uniben.edu>
2026-07-01 19:36:57 +0100

    Complete Chapter 1 student profile project

---
+++ student_profile.py
@@ -0,0 +1,23 @@
+student_name = "Light the Steminist"
+age = 21
+course = "Computer Science"
+school = "University of Benin"
+current_Level = 300
+favourite_Programming_Language = "Python" 
+total_hours_studying_in_computer_science = 4 * 365 * 30
+years_until_signout = 3 + 1
+total_hours_extracurriculars = 40 - 30
+age_next_two_years = age + 2
+seperator = "=================================="
+
+print (seperator,"\n",
+        "Name:", student_name, "\n", 
+        "Age:", age, "\n",
+        "Course:", course, "\n", 
+        "School:", school, "\n", "\n", "\n",
+
+
+        "Next Two Year's Age:", age_next_two_years, "\n" ,
+        "Study hours throughoyt of the year:", total_hours_studying_in_computer_science, "\n" 
+        , "\n",
+        seperator) 
